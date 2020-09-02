cf613c557a8ecd2c2a6a31a6ccf2c9196fb7067f
KaroW97 <karolina.wiaderek1997@gmail.com>
2020-09-02 22:39:55 +0200

    Change margin-bottom when there is no element in short shopping list

--- public/index.js
+++ public/index.js
@@ -9,7 +9,12 @@ $(document).ready(function(){
         $('.company-square').css("animation", "1s ease-out slideInBigSquare");
     }
 
-
+    if($('.list-content').length  == 0 ){
+                
+        $('.shopping-list-out').css('opacity','0')
+        $('.pageContentWrapper').css('margin-bottom','80px')
+      
+    }
     
     //If Width Less Then 1079 show list on the bottom if grater then 180 dont display if clicked is not true 
     $(window).resize(function(){
@@ -18,7 +23,7 @@ $(document).ready(function(){
            
             if($('.list-content').length  == 0 ){
                 
-                $('.center-shopping-list').css('opacity','0')
+                $('.shopping-list-out').css('opacity','0')
                 $('.pageContentWrapper').css('margin-bottom','80px')
               
             }else{
